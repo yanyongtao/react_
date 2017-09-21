47fe4d333c74f666a531a8fd5d845a0780e6f862
yanyongtao <2694291522@qq.com>
2017-09-21 11:34:35 +0800

    20170921

---
+++ react_webpack_yyt/index.js
@@ -0,0 +1,17 @@
+class Test extends React.Component {
+    constructor(...args) {
+        super(...args);
+        this.state={value:''}
+    }
+    fn(){
+        this.setState({
+            value:this.refs['txt1'].value
+        })
+    }
+    render(){
+        return <div>
+            <input ref="txt1" type="text" value="按钮" onChange={this.fn.bind(this)}/>
+            <span>{this.state.value}</span>
+        </div>;
+    }
+}
